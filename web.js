e5c2cd337d6d89a6009ccee90731590c8118c961
Ramunas <ramunas.daukas@gmail.com>
2013-08-29 06:54:57 -0700

    naujas failu pateikimas node js

--- web.js
+++ web.js
@@ -1,22 +1,53 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
 
-fs = require('fs');
-fs.readFile('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
+http.createServer(function(req, res) {
+  var uri = url.parse(req.url).pathname;
+    console.log(uri);
+    if (uri === "/"){
+	uri = "/index.html";
+    }
+  var filename = path.join(process.cwd(), unescape(uri));
+  var stats;
+
+  try {
+    stats = fs.lstatSync(filename); // throws if path doesn't exist
+  } catch (e) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found\n');
+    res.end();
+    return;
   }
-  //console.log(data);
-  app.get('/', function(request, response) {                                                         
-      response.send(data);
-  });
- 
-});
 
 
+  if (stats.isFile()) {
+    // path exists, is a file
+    var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+    res.writeHead(200, {'Content-Type': mimeType} );
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    var fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    // path exists, is a directory
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Index of '+uri+'\n');
+    res.write('TODO, show index?\n');
+    res.end();
+  } else {
+    // Symbolic link, other?
+    // TODO: follow symlinks?  security?
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('500 Internal server error\n');
+    res.end();
+  }
+
+}).listen(5000);
